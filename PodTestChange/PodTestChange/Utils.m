eb473415f4713573b7cb196726beb6169ee98369
siqingchan <siqingchan_ajk@58ganji.com>
2018-02-01 11:07:02 +0800

    完善button布局设置

--- PodTestChange/PodTestChange/Utils.m
+++ PodTestChange/PodTestChange/Utils.m
@@ -10,30 +10,44 @@
 @implementation Utils
 
 //设置button的内部图文布局
-+ (void)makeButtonContainerLayout:(UIButton *)button withImagePosition:(UIImageOrientation)orientation withMargin:(CGFloat)margin {
++ (void)makeButtonContainerLayout:(UIButton *)button withImagePosition:(UIImageOrientation)orientation withMargin:(CGFloat)space {
     //延时操作，为了让label和imageview获得frame ——Masonry在布局的时候，控件获得frame需要小小的延迟
+    __block CGFloat margin = space;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        CGSize imageSize = button.imageView.frame.size;
-        CGSize labelSize = button.titleLabel.frame.size;
+        CGRect imageRect = button.imageView.frame;
+        CGRect labelRect = button.titleLabel.frame;
+        CGRect buttonRect = button.frame;
         switch (orientation) {
             case UIImageOrientationDown: {
-                button.imageEdgeInsets = UIEdgeInsetsMake(imageSize.height/2+margin/2, 0, -imageSize.height/2-margin/2, -labelSize.width);
-                button.titleEdgeInsets = UIEdgeInsetsMake(-labelSize.height/2-margin/2, -imageSize.width, labelSize.height/2+margin/2, 0);
+                if (margin > buttonRect.size.height - imageRect.size.height - labelRect.size.height) {
+                    margin = buttonRect.size.height - imageRect.size.height - labelRect.size.height;
+                }
+                button.imageEdgeInsets = UIEdgeInsetsMake(labelRect.size.height/2+margin/2, -(imageRect.size.width/2+imageRect.origin.x-buttonRect.size.width/2), -labelRect.size.height/2-margin/2, imageRect.size.width/2+imageRect.origin.x-buttonRect.size.width/2);
+                button.titleEdgeInsets = UIEdgeInsetsMake(-imageRect.size.height/2-margin/2, -(labelRect.origin.x-buttonRect.size.width/2+labelRect.size.width/2), imageRect.size.height/2+margin/2, labelRect.origin.x-buttonRect.size.width/2+labelRect.size.width/2);
             }
                 break;
             case UIImageOrientationLeft: {
+                if (margin > buttonRect.size.width - imageRect.size.width - labelRect.size.width) {
+                    margin = buttonRect.size.width - imageRect.size.width- labelRect.size.width;
+                }
                 button.imageEdgeInsets = UIEdgeInsetsMake(0, -margin/2, 0, margin/2);
                 button.titleEdgeInsets = UIEdgeInsetsMake(0, margin/2, 0, -margin/2);
             }
                 break;
             case UIImageOrientationUp: {
-                button.imageEdgeInsets = UIEdgeInsetsMake(-imageSize.height/2-margin/2, 0, imageSize.height/2+margin/2, -labelSize.width);
-                button.titleEdgeInsets = UIEdgeInsetsMake(labelSize.height/2+margin/2, -imageSize.width, -labelSize.height/2-margin/2, 0);
+                if (margin > buttonRect.size.height - imageRect.size.height - labelRect.size.height) {
+                    margin = buttonRect.size.height - imageRect.size.height - labelRect.size.height;
+                }
+                button.imageEdgeInsets = UIEdgeInsetsMake(-labelRect.size.height/2-margin/2, -(imageRect.size.width/2+imageRect.origin.x-buttonRect.size.width/2), labelRect.size.height/2+margin/2, imageRect.size.width/2+imageRect.origin.x-buttonRect.size.width/2);
+                button.titleEdgeInsets = UIEdgeInsetsMake(imageRect.size.height/2+margin/2, -(labelRect.origin.x-buttonRect.size.width/2+labelRect.size.width/2), -imageRect.size.height/2-margin/2, labelRect.origin.x-buttonRect.size.width/2+labelRect.size.width/2);
             }
                 break;
             case UIImageOrientationRight: {
-                button.imageEdgeInsets = UIEdgeInsetsMake(0, labelSize.width+margin/2, 0, -labelSize.width-margin/2);
-                button.titleEdgeInsets = UIEdgeInsetsMake(0, -imageSize.width-margin/2, 0, imageSize.width+margin/2);
+                if (margin > buttonRect.size.width - imageRect.size.width - labelRect.size.width) {
+                    margin = buttonRect.size.width - imageRect.size.width- labelRect.size.width;
+                }
+                button.imageEdgeInsets = UIEdgeInsetsMake(0, labelRect.size.width+margin/2, 0, -labelRect.size.width-margin/2);
+                button.titleEdgeInsets = UIEdgeInsetsMake(0, -imageRect.size.width-margin/2, 0, imageRect.size.width+margin/2);
             }
                 break;
             default:
